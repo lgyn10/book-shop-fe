57f9ee315b758679eed1a82468fdcc586d526000
ChanGyunLee <lgyn10@naver.com>
2024-07-12 11:01:10 +0900

    feat: sanitize 및 stlyed-components 글로벌 스타일링
    기존에 사용하던 pico.css 삭제
    sanitize 패키지 설치
    stlyed-components 패키지 설치
    
    GlobalStyle 선언

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,18 @@
+import styled from 'styled-components';
+
 const Header = () => {
   return (
-    <header>
+    <StyledHeader>
       <h1>book store</h1>
-    </header>
+    </StyledHeader>
   );
 };
 
 export default Header;
+
+const StyledHeader = styled.header`
+  background-color: #333;
+  h1 {
+    color: wheat;
+  }
+`;
