fc53b8fd3a8297586fc84cc9e4919c3d74881834
ChanGyunLee <lgyn10@naver.com>
2024-07-13 22:32:27 +0900

    feat: Layout 컴포넌트의 children 스타일링

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import Footer from '../common/Footer';
 import Header from '../common/Header';
 
@@ -9,10 +10,17 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <StyledLayout>{children}</StyledLayout>
       <Footer />
     </>
   );
 };
 
 export default Layout;
+
+const StyledLayout = styled.div`
+  width: 100%; // 반응형을 위해 100%로 설정
+  margin: 0 auto; // 가운데 정렬을 위해 margin을 auto로 설정
+  max-width: ${({ theme }) => theme.layout.width.large}; // 최대 너비를 theme에서 가져옴
+  padding: 20px 0;
+`;
