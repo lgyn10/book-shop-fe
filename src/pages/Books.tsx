f2480a814a79d77320377bdd45aba15edacad342
ChanGyunLee <lgyn10@naver.com>
2024-07-17 08:18:42 +0900

    feat: Books 컴포넌트 구조 생성

---
+++ src/pages/Books.tsx
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components';
+import Title from '../components/common/Title';
+
+const Books = () => {
+  return (
+    <>
+      <Title size='large'>도서 검색 결과</Title>Books
+    </>
+  );
+};
+
+export default Books;
+
+const StyledBooks = styled.div``;
