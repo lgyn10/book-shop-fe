0a1dcaa6e69b668c175b1ff6ca37b67aeba18a0d
ChanGyunLee <lgyn10@naver.com>
2024-07-17 10:39:24 +0900

    chore: getImgSrc 사이즈 변경

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,4 +1,4 @@
 // getImgSrc : 이미지 id를 받아서 이미지 주소를 반환하는 함수
 export const getImgSrc = (id: number) => {
-  return `https://picsum.photos/id/${id}/200/200`;
+  return `https://picsum.photos/id/${id}/600/600`;
 };
