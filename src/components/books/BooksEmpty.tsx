3ed0c4b9fe3157eb9af1d3ec6f522a796aebc960
ChanGyunLee <lgyn10@naver.com>
2024-07-17 11:16:23 +0900

    feat: BooksEmpty 컴포넌트 구현

--- src/components/books/BooksEmpty.tsx
+++ src/components/books/BooksEmpty.tsx
@@ -1,5 +1,35 @@
+import { FaSmile } from 'react-icons/fa';
+import { styled } from 'styled-components';
+import Title from '../common/Title';
+
 const BooksEmpty = () => {
-  return <div>BooksEmpty</div>;
+  return (
+    <StyledBooksEmpty>
+      <div className='icon'>
+        <FaSmile />
+      </div>
+      <Title size={'large'} color='secondary'>
+        검색결과가 없습니다.
+      </Title>
+    </StyledBooksEmpty>
+  );
 };
 
 export default BooksEmpty;
+
+const StyledBooksEmpty = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  padding: 120px 0;
+
+  .icon {
+    svg {
+      font-size: 4rem;
+      fill: #ccc; // fill 속성: svg의 색상을 변경
+      color: ${({ theme }) => theme.color.primary};
+    }
+  }
+`;
