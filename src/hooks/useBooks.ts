82414b221ae9005e2fa28d9e42c171ab9c29aab5
ChanGyunLee <lgyn10@naver.com>
2024-07-17 21:09:51 +0900

    refactor: res를 구조분해할당으로 리팩토링

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -23,10 +23,10 @@ export const useBooks = () => {
     const limit = LIMIT;
 
     // fetchBooks 함수를 호출하여 책 목록을 가져옴
-    fetchBooks({ categoryId, news, currentPage, limit }).then((res) => {
-      setBooks(res.books); // 책 목록을 업데이트
-      setPagination(res.pagination); // 페이징 정보를 업데이트
-      setIsEmpty(res.books.length === 0); // 책 목록이 비어있는지 확인
+    fetchBooks({ categoryId, news, currentPage, limit }).then(({ books, pagination }) => {
+      setBooks(books); // 책 목록을 업데이트
+      setPagination(pagination); // 페이징 정보를 업데이트
+      setIsEmpty(books.length === 0); // 책 목록이 비어있는지 확인
     });
   }, [location.search]);
 
