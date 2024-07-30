06f0e4501995b8b66164bfafc7c4813a3f7b96ef
ChanGyunLee <lgyn10@naver.com>
2024-07-30 11:04:25 +0900

    refactor: 라우팅 코드 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -13,83 +13,66 @@ import OrderList from './pages/OrderList';
 import ResetPassword from './pages/ResetPassword';
 import Signup from './pages/Signup';
 
+// {
+//   path: '/',
+//   element: (
+//     <Layout>
+//       <Home />
+//     </Layout>
+//   ), // Home 컴포넌트를 렌더링, Layout 컴포넌트로 감싸기 - 공통 레이아웃 적용
+//   errorElement: <Error />, // 에러 페이지 설정
+//   // errorElement를 설정하지 않으면 기본 에러 페이지가 렌더링 - react-router-dom의 Route 컴포넌트의 기본 에러 페이지
+// },
+
 // createBrowserRouter 함수를 사용하여 라우터를 설정
-const router = createBrowserRouter([
+const routeList = [
   {
     path: '/',
-    element: (
-      <Layout>
-        <Home />
-      </Layout>
-    ), // Home 컴포넌트를 렌더링, Layout 컴포넌트로 감싸기 - 공통 레이아웃 적용
-    errorElement: <Error />, // 에러 페이지 설정
-    // errorElement를 설정하지 않으면 기본 에러 페이지가 렌더링 - react-router-dom의 Route 컴포넌트의 기본 에러 페이지
+    element: <Home />,
   },
   {
     path: '/books',
-    element: (
-      <Layout>
-        <Books />
-      </Layout>
-    ),
+    element: <Books />,
   },
   {
     path: '/signup',
-    element: (
-      <Layout>
-        <Signup />
-      </Layout>
-    ),
+    element: <Signup />,
   },
   {
     path: '/reset',
-    element: (
-      <Layout>
-        <ResetPassword />
-      </Layout>
-    ),
+    element: <ResetPassword />,
   },
   {
     path: '/login',
-    element: (
-      <Layout>
-        <Login />
-      </Layout>
-    ),
+    element: <Login />,
   },
   {
     path: '/book/:bookId',
-    element: (
-      <Layout>
-        <BookDetail />
-      </Layout>
-    ),
+    element: <BookDetail />,
   },
   {
     path: '/carts',
-    element: (
-      <Layout>
-        <Cart />
-      </Layout>
-    ),
+    element: <Cart />,
   },
   {
     path: '/order',
-    element: (
-      <Layout>
-        <Order />
-      </Layout>
-    ),
+    element: <Order />,
   },
   {
     path: '/orderlist',
-    element: (
-      <Layout>
-        <OrderList />
-      </Layout>
-    ),
+    element: <OrderList />,
   },
-]);
+];
+
+const router = createBrowserRouter(
+  routeList.map((route) => {
+    return {
+      ...route,
+      element: <Layout>{route.element}</Layout>,
+      errorElement: <Error />,
+    };
+  })
+);
 
 function App() {
   return (
